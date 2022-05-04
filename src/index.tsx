f9bdefc98e32454db3b6b04d3a723643e1141095
wilson.ferreira <willferreirajr@icloud.com>
2022-05-04 16:12:39 -0300

    feat: Cria pagina NewRoom e modifica index

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 // import './services/firebase';
 
+import './styles/global.scss';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
