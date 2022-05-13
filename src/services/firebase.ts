11ce301b470d4bc6b59e351c554ba8b3d4846e0d
wilson.ferreira <willferreirajr@icloud.com>
2022-05-13 09:06:11 -0300

    feat: Finalização do projeto

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,8 +1,7 @@
 import firebase from 'firebase/app'
 
-
-
 import 'firebase/auth';
+import 'firebase/database'
 import 'firebase/firestore';
 
 const firebaseConfig = {
@@ -11,14 +10,11 @@ const firebaseConfig = {
     databaseURL: process.env.REACT_APP_DATABASE_URL,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDE_ID,
     appId: process.env.REACT_APP_APP_ID
 };
 
-console.log(firebaseConfig)
-
 firebase.initializeApp(firebaseConfig);
-
 
 export const auth = firebase.auth();
 
